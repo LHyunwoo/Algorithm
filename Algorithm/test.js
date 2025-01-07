5bf34d5c2b63fd0b17292fc68e1bf0ddc5d49e62
hyunwoo <gusdndls@onpoom.co.kr>
2025-01-07 14:17:00 +0900

    base.js

--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,4 +1,6 @@
-let input = require('fs').readFileSync('input.txt').toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
 var a = parseInt(input[0]);
 var b = parseInt(input[1]);
 console.log(a + b);
